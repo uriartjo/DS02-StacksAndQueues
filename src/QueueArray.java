f08692f5ed29b17f473689eb74cfc6d2e36a49e3
Joey <joey.uriarte@saintandrews.net>
2022-10-04 10:38:45 -0400

    Initial commit

---
+++ src/QueueArray.java
@@ -0,0 +1,16 @@
+package com.company;
+
+public class QueueArray {
+}
+
+
+
+
+/*
+E enqueue(E item) - Pushes an item onto the bottom/back of this queue.
+E dequeue() - Removes the object at the top of this queue and returns that object as the value of this function.
+E peek()- Looks at the object at the top of this queue without removing it from the queue.
+void display() - Outputs the queue order
+int size() - Returns number of items in the queue
+boolean isEmpty - Checks if queue is currently empty
+ */
---
+++ src/StackArray.java
@@ -0,0 +1,15 @@
+package com.company;
+
+public class StackArray {
+
+}
+
+
+/*
+E push(E item) - Pushes an item onto the top of this stack.
+E pop() - Removes the object at the top of this stack and returns that object as the value of this function.
+E peek()- Looks at the object at the top of this stack without removing it from the stack.
+void display() - Outputs the stack order
+int size() - Returns number of items in the stack
+boolean isEmpty - Checks if stack is currently empty
+ */
