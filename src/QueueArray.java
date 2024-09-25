77fdf49333c9f28ed0346183a0c92625e132c2a7
uriartjo <40124152+uriartjo@users.noreply.github.com>
2024-09-25 11:29:15 -0400

    Added constructor requirements for Array-based Stack/Queue

--- src/QueueArray.java
+++ src/QueueArray.java
@@ -6,6 +6,9 @@ public class QueueArray {
 
 
 /*
+The constructor should take in an integer that represents the number of items the Queue will hold.
+If more items are added beyond capacity, have it dequeue the next element and then enqueue the one being added
+
 E enqueue(E item) - Pushes an item onto the bottom/back of this queue.
 E dequeue() - Removes the object at the top of this queue and returns that object as the value of this function.
 E peek()- Looks at the object at the top of this queue without removing it from the queue.
