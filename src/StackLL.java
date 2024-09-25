98c41c685d6800f145685043798ab0342d105df7
uriartjo <40124152+uriartjo@users.noreply.github.com>
2024-09-25 11:29:44 -0400

    Added constructor requirements for Array-based Stack/Queue

--- src/StackLL.java
+++ src/StackLL.java
@@ -3,9 +3,6 @@ public class StackLL {
 
 
 /*
-The constructor should take in an integer that represents the number of items the Stack will hold.
-If more items are added beyond capacity, have it pop the next element and then push the one being added
-
 E push(E item) - Pushes an item onto the top of this stack.
 E pop() - Removes the object at the top of this stack and returns that object as the value of this function.
 E peek()- Looks at the object at the top of this stack without removing it from the stack.
